363077e33fb8a817e1354a15a1113fc509fb91d4
krgirard33 <ken.girard@gmail.com>
2022-11-06 09:52:33 -0600

    Finished test & creation of Compartment class

--- compartment.js
+++ compartment.js
@@ -9,12 +9,21 @@ class Compartment {
 	// 6 - Add an items parameter and set an items property with it
 	// 7 - Throw an error if any of the items in the array are not instances of the Item class (hint: write a method to help with this)
 	// 8 - Give the items parameter a default value of an empty array
-	constructor() {
+	constructor(items=[]){
+		this.items = items;
+		if (this.hasInvalidItem()) {
+			throw Error("All items must be valid instances of the Item class.");
+		}
 
+		// Code method for #7 here
+		hasInvalidItem() {
+			for (let i=0; i<this.items.length; i++) {
+				if (!(this.items[i] instanceOf Item)) {
+					return true;
+				}
+			}
+		}
 	}
-
-	// Code method for #7 here
-
 }
 
 module.exports = Compartment;
--- spec/compartment.spec.js
+++ spec/compartment.spec.js
@@ -1,20 +1,31 @@
 const Item = require('../item.js');
 const Compartment = require('../compartment.js');
 
-describe('The Compartment class', function() {
+describe('The Compartment class', function () {
 
 	// SPEC 6 - What happens when an array of valid Item-class objects are passed in?
-	it('should correctly instantiate a compartment if an array of Item-class objects is passed in', function() {
-
+	it('should correctly instantiate a compartment if an array of Item-class objects is passed in', function () {
+		let notebook = new Item("notebook", "main");
+		let binder = new Item("binder", "main");
+		let main = new Compartment([notebook, binder]);
+		expect(main).toBeInstanceOf(Compartment);
+		expect(main.items[0]).toBeInstanceOf(Item);
+		expect(main.items[1]).toBeInstanceOf(Item);
 	});
 
 	// SPEC 7 - What happens when one or more items in the array that is passed in are not valid instances of the Item class?
-	it('should throw an error if any of the items passed in are not instances of the Item class', function() {
-
+	it('should throw an error if any of the items passed in are not instances of the Item class', function () {
+		let instantiationAttempt = function () {
+			new Compartment([{ whatAmI: "regular JS object" }]);
+		}
+		let errorMessage = "All items must be valid instances of the Item class.";
+		expect(instantiationAttempt).toThrowError(errorMessage);
 	});
 
 	// SPEC 8 - What happens when no arguments are passed in?
-	it('should instantiate a new compartment object with an empty array as the value of the items property', function() {
-
+	it('should instantiate a new compartment object with an empty array as the value of the items property', function () {
+		let main = new Compartment();
+		expect(main).toBeInstanceOf(Compartment);
+		expect(main.items).toEqual([]);
 	});
 });
